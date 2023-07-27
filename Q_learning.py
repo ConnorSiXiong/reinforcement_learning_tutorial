492607a0b319135b466f22c086e6fc1d991771e3
xiong si <2428545784@qq.com>
2023-07-28 01:02:35 +1200

    fix env setting to slippery=True

--- Q_learning.py
+++ Q_learning.py
@@ -2,8 +2,9 @@ import gymnasium as gym
 import random
 import numpy as np
 
-env = gym.make('FrozenLake-v1', desc=None, map_name="4x4", is_slippery=False)
-seed = 20
+np.set_printoptions(suppress=True)
+env = gym.make('FrozenLake-v1', desc=None, map_name="4x4", is_slippery=True)
+
 observation, info = env.reset()
 
 direction_dic = {0: "Move left",
@@ -22,28 +23,11 @@ alpha = 0.1
 gamma = 0.9
 
 
-def choose_action_training(state):
-    if Q_table[state].sum() == 0 or np.random.uniform() > 0.9:
-        while True:
-            a = env.action_space.sample()
-            _, s, r, _ = env.P[state][a][0]
-            if s != state:
-                break
-    else:
-        a = np.argmax(Q_table[state])
-    return int(a)
-
-
 def choose_action(state):
-    if Q_table[state].sum() == 0:
-        while True:
-            a = np.random.randint(num_actions)
-            _, s, r, _ = env.P[state][a][0]
-            if s != state:
-                break
+    if Q_table[state].sum() == 0 or np.random.uniform() > 0.9:
+        a = env.action_space.sample()
     else:
-        a = np.argmax(Q_table[state])
-
+        a = np.argmax(Q_table[state, :])
     return int(a)
 
 
@@ -53,53 +37,40 @@ game_round = 0
 pass_moves = []
 pass_times = 0
 total_times = 0
-
+r1 = 0.9
+r2 = 0.99
 # training
 for i in range(100000):
-    game_round += 1
-    cur_state = observation
-    action = choose_action_training(cur_state)
-    next_state, reward, terminated, truncated, _ = env.step(action)
+    state, info = env.reset()
+    done = False
+    r1 *= r2
+    while not done:
+        action = choose_action(state)
 
-    reward = float(reward)
+        next_state, reward, terminated, truncated, _ = env.step(action)
 
-    Q_table[cur_state, action] = Q_table[cur_state, action] + alpha * (
-            reward + gamma * Q_table[next_state, :].max() - Q_table[cur_state, action])
-    # action = env.action_space.sample()  # agent policy that uses the observation and info
+        reward = float(reward)
 
-    observation = next_state
-    if terminated or truncated:
-        observation, info = env.reset()
+        Q_table[state, action] = Q_table[state, action] + alpha * (
+                reward + gamma * Q_table[next_state, :].max() - Q_table[state, action])
+
+        state = next_state
+        if terminated or truncated:
+            done = True
 
 print(Q_table)
 
-observation, info = env.reset()
-moves = []
-cur_moves = []
-for i in range(100000):
-    if observation == 0:
-        cur_moves = []
+success_times = 0
+for i in range(1000):
+    state, info = env.reset()
+    done = False
+    while not done:
+        action = np.argmax(Q_table[state])
 
-    game_round += 1
-    action = choose_action(observation)
-    cur_moves.append(direction_dic[action])
-    # action = env.action_space.sample()  # agent policy that uses the observation and info
-    observation, reward, terminated, truncated, info = env.step(action)
-    # print('observation', observation)
-    if terminated or truncated or game_round == 50:
-        observation, info = env.reset()
-        total_times += 1
-        moves.append(cur_moves)
-        if reward == 1:
-            pass_moves.append(game_round)
-            pass_times += 1
-        game_round = 0
-
-env.close()
-from statistics import mean
-
-# print(mean(pass_moves))
-print(mean(pass_moves))
-print('pass_times', pass_times)
-print('total_times', total_times)
-print(moves[100])
+        next_state, reward, terminated, truncated, _ = env.step(action)
+        state = next_state
+        if terminated or truncated or reward == 1:
+            done = True
+            if reward == 1:
+                success_times += 1
+print(success_times)
