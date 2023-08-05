a4eb38c6edde9b2e14cbc91213f6ac5435a18330
xiong si <2428545784@qq.com>
2023-08-05 15:24:33 +1200

    make it a playground for experiment and understand the env

--- DQN/breakout_playground.py
+++ DQN/breakout_playground.py
@@ -10,20 +10,44 @@ ACTIONS = [0, 1, 2, 3]
 
 
 # Play the game for a few steps
-for _ in range(100000):
+for i in range(100000):
     # Choose a random action (you can implement a RL agent later to select actions intelligently)
     action = env.action_space.sample()
 
     # Perform the action and observe the outcome
     observation, reward, terminated, truncated, info = env.step(action)
+    print('observation', observation.shape)
     print('reward', reward)
     print("terminated", terminated)
     print("truncated", truncated)
     print('info', info)
+    if reward:
+        print('xxx reward', reward)
     # Render the game (optional, you can comment this out to run the game in the background)
 
     # Check if the game is over (done=True) and reset if necessary
     if terminated or truncated:
+        print('observation', observation.shape)
+        print(observation)
+        print('reward', reward)
+        print("aaa terminated", terminated)
+        print("aaa truncated", truncated)
+        print('info', info)
         env.reset()
 # Close the environment when done
 env.close()
+
+from ReplayMemory import ReplayMemory
+from DQN_Architecture import DQN
+
+N = 1000000
+k_frames = 4
+game_frame_height = 84
+game_frame_width = 84
+game_num_actions = 4
+target_Q_update_steps = 10000
+batch = 32
+# if __name__ == "__main__":
+#     D = ReplayMemory(N)
+#     Q = DQN(game_num_actions, k_frames, game_frame_height, game_frame_width)
+#     target_Q = DQN(game_num_actions, k_frames, game_frame_height, game_frame_width)
