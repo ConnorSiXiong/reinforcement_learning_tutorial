3e217d71cbab92061a99b324b8ba05429667cb8d
xiong si <2428545784@qq.com>
2023-08-08 00:11:47 +1200

    add down_sampling and make the funs into one

--- DQN/ImageProcessing.py
+++ DQN/ImageProcessing.py
@@ -9,5 +9,16 @@ def grayscale(image):
     return np.mean(image, axis=2, keepdims=False)
 
 
+def down_sampling(image):
+    return image[::2, ::2]
+
+
 def normalization(image):
     return image / 256
+
+
+def process_image(image):
+    image = clipping(image)
+    image = grayscale(image)
+    image = down_sampling(image)
+    return normalization(image)
